1d4418ec53eaab0c70aa532d6ee3346a5a2dd181
Christian Gregersen(github) <awear@appsyouwear.com>
2025-11-25 01:46:04 +0100

    dagger

--- ci/shared-dagger.zsh
+++ ci/shared-dagger.zsh
@@ -1,7 +1,4 @@
-dagger -m
-dagger -m github.com/jczz/shared/ci/dagger call deploy
-
 go build ./...
 
-# dagger
 dagger -m ./ci/dagger functions
+dagger -m github.com/jczz/shared/ci/dagger call deploy
